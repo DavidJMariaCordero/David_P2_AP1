9baff43dbf4d7669f50eb01fd8ff03e792b8a630
David Maria <eldavid_46@hotmail.com>
2020-11-02 16:11:08 -0400

    Inicial

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace David_P2_AP1.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite(@"Data Source = Data/Parcial.db");
+        }
+    }
+}
---
+++ UI/Registros/Registro.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace David_P2_AP1.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for Registro.xaml
+    /// </summary>
+    public partial class Registro : Window
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+    }
+}
